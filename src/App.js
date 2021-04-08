e045c523694e7fd7e115f82b077508d37eaba2c2
Sabrina Sumona <snsbauet04@gmail.com>
2021-04-08 22:25:29 +0600

    component tree generate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MainComponent from './components/MainComponent';
 
 function App() {
   return (
     <div className="App">
-
+      <MainComponent />
     </div>
   );
 }
