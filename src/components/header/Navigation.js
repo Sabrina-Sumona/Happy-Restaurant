5d4ac43d14b02323355a8748e50bdadf5f0f28cf
Sabrina Sumona <snsbauet04@gmail.com>
2021-04-18 00:00:38 +0600

    designing navigation bar

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-const Navigation = () => {
-    return (
-        <div>
-            <Navbar dark color='dark'>
-                <div className="container">
-                    <NavbarBrand href="/">
-                        Happy Restaurant
-                    </NavbarBrand>
-                </div>
-            </Navbar>
-        </div>
-    );
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Nav, NavItem, NavbarToggler, Collapse } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false
+            // isNavOpen: true
+        }
+    }
+
+    navToggle = () => {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        })
+    }
+    render() {
+        return (
+            <div>
+                <Navbar dark color='dark' expand="sm">
+                    <div className="container">
+                        <NavbarToggler onClick={this.navToggle} />
+                        <NavbarBrand href="/">
+                            Happy Restaurant
+                        </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav className="mr-auto" navbar>
+                                <NavItem>
+                                    <Link to="/" className="nav-link active">Home</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to="/menu" className="nav-link">Menu</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to="/about" className="nav-link">About</Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link to="/contact" className="nav-link">Contact</Link>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </div>
+                </Navbar>
+            </div >
+        );
+    }
 }
 
 export default Navigation;
