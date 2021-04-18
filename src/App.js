543f713c5e7ce7b58699cc796fa0f56ae8b49b29
Sabrina Sumona <snsbauet04@gmail.com>
2021-04-19 04:56:46 +0600

    using redux in react app: creating store & connecting to react app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import MainComponent from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import myStore from './redux/store';
+import { Provider } from 'react-redux';
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <MainComponent />
-      </BrowserRouter>
+      <Provider store={myStore}>
+        <BrowserRouter>
+          <MainComponent />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,7 +3,8 @@ import COMMENTS from '../data/comments';
 
 const initialState = {
     dishes: DISHES,
-    comments: COMMENTS
+    comments: COMMENTS,
+    sample: "Owner of Happy Restaurant is Sabrina Sumona"
 }
 
 export const Reducer = (state = initialState, action) => {
