2c62ebea431dab615aaf027815bc40ea2bb31eec
Sabrina Sumona <snsbauet04@gmail.com>
2021-04-19 04:47:48 +0600

    using redux in react app: reducer added

---
+++ src/redux/reducer.js
@@ -0,0 +1,11 @@
+import DISHES from '../data/dishes';
+import COMMENTS from '../data/comments';
+
+const initialState = {
+    dishes: DISHES,
+    comments: COMMENTS
+}
+
+export const Reducer = (state = initialState, action) => {
+    return state;
+}
