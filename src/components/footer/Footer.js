ad291625adf313e6fa5ba534687f58548388472c
Sabrina Sumona <snsbauet04@gmail.com>
2021-04-18 20:09:26 +0600

    footer component design

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-
+        <div className="footer">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-auto">
+                        <p>Happy Restaurant &#169; Copyright 2021</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
